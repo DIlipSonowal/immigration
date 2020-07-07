5f5cafa33ca4dc087ed64a2d917d8a011b111642
9880649203 <ashwinibj7@gmail.com>
2020-07-07 23:43:32 +0530

    login changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './components/login/login.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent }
+  { path: '', component: HomeComponent, children:[
+    
+  ] },
+  { path: 'login', component: LoginComponent }
 ];
 
 @NgModule({
