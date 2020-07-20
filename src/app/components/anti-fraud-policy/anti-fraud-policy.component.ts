5425829a782a674a988365c6f5a5d5c81f9d2b16
9880649203 <ashwinibj7@gmail.com>
2020-07-20 22:16:12 +0530

    changes added

---
+++ src/app/components/anti-fraud-policy/anti-fraud-policy.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-anti-fraud-policy',
+  templateUrl: './anti-fraud-policy.component.html',
+  styleUrls: ['./anti-fraud-policy.component.scss']
+})
+export class AntiFraudPolicyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
